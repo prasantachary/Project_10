a61d3581c8ca941339f98fc5f649a7e5132ac65b
P. Prasant Achary <150173869+prasantachary@users.noreply.github.com>
2023-12-12 00:46:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def prasant(request):
+    return render(request,'prasant.html')
+
+def kanha(request):
+    return render(request,'kanha.html')
+
+def life(request):
+    return render(request,'life.html')
